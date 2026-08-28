b4f0dfd8bc1db18af5e76a159b7625fbedd4f2dd
Repentancevrai <cabygdesing@gmail.com>
2026-08-28 11:56:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Never put a Supabase service_role key in this file.
 
 window.RENDIS_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE",
+  SUPABASE_URL: "https://colewahriqfeubfneseq.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvbGV3YWhyaXFmZXViZm5lc2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNzg2NzMsImV4cCI6MjEwMjk1NDY3M30.wnO9nznfleQES6y3Oj4rLhjylrhE-X3lACh4VJb-vVY",
   GOAL_FCFA: 60000000
 };
