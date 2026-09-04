1ff64e16e4735658ee21458be2a12d3f240c6b3a
Repentancevrai <cabygdesing@gmail.com>
2026-09-04 10:53:15 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 window.RENDIS_CONFIG = {
   SUPABASE_URL: "https://colewahriqfeubfneseq.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvbGV3YWhyaXFmZXViZm5lc2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNzg2NzMsImV4cCI6MjEwMjk1NDY3M30.wnO9nznfleQES6y3Oj4rLhjylrhE-X3lACh4VJb-vVY",
-  GOAL_FCFA: 60000000
+  GOAL_FCFA: 60000000,DJAMO_BUSINESS_PAYMENT_LINK: "https://pay.djamo.com/0abov"
 };
