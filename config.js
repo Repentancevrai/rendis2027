9bc2bb6e2ba0a2344cc2f451a1be0014ad426493
Repentancevrai <cabygdesing@gmail.com>
2026-09-17 06:29:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.RENDIS_CONFIG = {
   SUPABASE_URL: "https://colewahriqfeubfneseq.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_BSb8y-lBZlMLPaT6mWyiDQ_1ZyIi4La",
   GOAL_FCFA: 60000000,
-  DJAMO_BUSINESS_PAYMENT_LINK: "https://pay.djamo.com/0abov",
+  DJAMO_BUSINESS_PAYMENT_LINK: "https://pay.djamo.com/y0olu",
   ORANGE_MONEY_NUMBER: "+225 07 48 96 16 24",
   WAVE_NUMBER: "+225 07 48 96 16 24"
 };
